0afcdc3b79b20f7087268f67b08b5bf351804373
Ludovic Boisnon <lboisnon@gmail.com>
2016-02-26 16:15:38 -0800

    styling

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -46,6 +46,6 @@ class PicturesController < ApplicationController
   end
 
   def picture_params
-    params.require(:picture).permit(:title,:description)
+    params.require(:picture).permit(:title,:description, :image)
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
 
-  has_attached_file :image, :styles: { :medium => "400x400#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, styles: { :medium => "800x800#", :thumb => "250x250#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
